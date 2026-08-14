9c47953601fa1f2cd89889c4d46242ba89c2fbb8
thenushans-magicktech <ThenushanS@MagickTech.com>
2026-08-14 15:29:12 +0530

    feat: add PWA install prompt component and required configuration

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -3,12 +3,14 @@
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner";
 import { ServiceWorkerRegister } from "@/components/providers/service-worker-register";
+import { InstallPrompt } from "@/components/providers/install-prompt";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ServiceWorkerRegister />
       {children}
+      <InstallPrompt />
       <Toaster
         position="top-right"
         theme="dark"
